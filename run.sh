e007702b31eda545e7bbe0e032b3e35d4f74dfb9
James Banting <jrbanting@gmail.com14244009+jbants@users.noreply.github.com>
2025-12-02 18:29:18 -0800

    Update dependencies and enhance run script
    This commit refines the project's dependencies by removing outdated or unused
    packages from requirements.txt, which helps streamline the environment setup
    and reduces potential conflicts. Additionally, the run.sh script has been
    modified to include the serverless backend option, Coiled, enhancing the
    capabilities for managing resource allocation and execution in a serverless
    environment.

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ python src/main.py \
     --end-date 2025-12-31 \
     --bbox -73.43 42.72 -71.47 45.02 \
     --storage-backend fsspec \
+    --serverless-backend coiled \
     --fsspec-uri s3://testing-embeddings-184161167200-ca-central-1/zarr_app/vermont
